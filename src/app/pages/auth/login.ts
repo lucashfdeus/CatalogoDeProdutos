f971afa0982d302a58ec512f85598c38e0587cf8
Lucas Hernique <lucas.hfdeus@gmail.com>
2025-08-20 10:07:46 -0300

    refactor: Ajuste label e placeholder para senha.

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -58,8 +58,8 @@ import { MessageService } from 'primeng/api';
                         <div>
                             <label for="email1" class="block text-surface-900 dark:text-surface-0 text-xl font-medium mb-2">Email</label>
                             <input pInputText id="email1" type="text" placeholder="Email" class="w-full md:w-120 mb-8" [(ngModel)]="email" />
-                            <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Password</label>
-                            <p-password id="password1" [(ngModel)]="password" placeholder="Password" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false"></p-password>
+                            <label for="password1" class="block text-surface-900 dark:text-surface-0 font-medium text-xl mb-2">Senha</label>
+                            <p-password id="password1" [(ngModel)]="password" placeholder="Senha" [toggleMask]="true" styleClass="mb-4" [fluid]="true" [feedback]="false"></p-password>
                             <div class="flex items-center justify-between mt-2 mb-8 gap-8">
                                 <div class="flex items-center">
                                     <p-checkbox [(ngModel)]="rememberMe" id="rememberme1" binary class="mr-2"></p-checkbox>
