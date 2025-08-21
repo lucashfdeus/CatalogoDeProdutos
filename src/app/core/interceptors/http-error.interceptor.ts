ec7d5180183c87df5e43056298d5fd098eb600f4
Lucas Hernique <lucas.hfdeus@gmail.com>
2025-08-21 11:11:55 -0300

    refactor: Ajuste mensagem retorno api

--- src/app/core/interceptors/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -19,6 +19,17 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           const customError = new Error('Servidor indisponível. Verifique se a API está rodando em http://localhost:5000');
           return throwError(() => customError);
         }
+
+        if (error.status === 400 && Array.isArray(error.error)) {
+          const errorMessages = error.error.map((err: { description: string; }) => err.description);
+          const combinedError = new Error(errorMessages.join('\n'));
+          return throwError(() => combinedError);
+        }
+
+        if (error.status === 400 && error.error?.description) {
+          return throwError(() => new Error(error.error.description));
+        }
+
         return throwError(() => error);
       })
     );
